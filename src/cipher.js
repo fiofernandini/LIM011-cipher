7e3b99f1f72806065163eb1099aa0a2d3d28c61d
fiofernandini <fiofernandini12@gmail.com>
2019-10-06 17:33:14 -0500

    modificando

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,29 @@
 window.cipher = {
-  // Acá escribe tu código.
+  encode: (string, offset) => {
+      //SE CREA UNA VARIABLE VACIA
+    let mensajecifrado = "";
+    // CON FOR HACEMOS UN RECORRIDO POR TODO EL STRING
+    for (let i = 0; i < string.length; i++) {
+  // CONDICION PARA EL CIFRADO DE MAYUSCULAS
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+  // SE APLICA LA FORMULA PARA OBTENER EL NUEVO ASCII
+          let ascii = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+  // EN LA VARIABLE VACIA SE INSERTA EL NUEVO ASCII CONVERTIDO A LETRA
+          mensajecifrado += String.fromCharCode(ascii);
+  //CONDICION PARA EL CIFRADO DE MINUSCULAS
+        } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+  //SE APLICA FORMULA PARA OBTENER EL NUEVO ASCII
+          let ascii = (string.charCodeAt(i) - 97 + offset) % 26 + 97;
+  //SE INSERTA EN LA VARIABLE VACIA EL NUEVO ASCII CONVERTIDO A LETRA
+          mensajecifrado += String.fromCharCode(ascii);
+        } else 
+  // CONDICION PARA LOS ESPACIOS
+      {if (string.charCodeAt(i)==32){
+  //SE INSERTA EN LA VARIABLE VACIA EL ESPACIO
+            mensajecifrado += String.fromCharCode(32)}
+        }
+    }
+    return mensajecifrado;
+  }
 };
+
--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
+document.getElementById('segundaPantalla').classList.add('ocultar');
+document.getElementById('pantallacifrar').classList.add('ocultar');
+
 document.getElementById('botonIngresar').addEventListener('click', function(){
-    document.getElementById('cabecera').style.display='none';
-    document.getElementById('pantalla1').style.display='none';
-    document.getElementById('derechos').style.display='none';
-    document.getElementById('segundaPantalla').style.display='block';
-    document.getElementById('pantallacifrar').style.display='block';
+    document.getElementById('cabecera').classList.add('ocultar');
+    document.getElementById('pantalla1').classList.add('ocultar');
+    document.getElementById('derechos').classList.add('ocultar');
+    document.getElementById('segundaPantalla').classList.add('mostrar');
+    document.getElementById('pantallacifrar').classList.add('mostrar');
     
 });
 
-  
+ 
 let mensaje = document.getElementById('mensajito');
 let desplaza = document.getElementById('desplaza');
 let resultado = document.getElementById('textarea2');
 
-
 //CIFRAR
+document.getElementById('boton1').addEventListener('click', function(){
+    //SE TOMA EL CONTENIDO DEL MENSAJE
+    let string = mensaje.value;
+    //SE TOMA EL CONTENIDO DE LOS DESPLAZAMIENTOS, CON parseInt SE ASEGURA QUE SEAN NUMEROS Y ENTEROS
+    let offset = parseInt(desplaza.value);
+    
+    resultado.innerHTML = cipher.encode(string, offset); });
+
+
+
+
+
+
+
+
+
+/*//MI PRIMER MODELO SOLO EN UN JS
 document.getElementById('boton1').addEventListener('click', function(){
     //SE TOMA EL CONTENIDO DEL MENSAJE
     let string = mensaje.value;
@@ -23,7 +42,7 @@ document.getElementById('boton1').addEventListener('click', function(){
     for(let i =0; i < string.length; i++){
     //CONSIDERACION DE LOS ESPACIOS
     if(string.charCodeAt(i)==32){
-    resultado.innerHTML += " ";}
+    resultado.innerHTML += String.fromCharCode(32);}
     //CIFRADO PARA MAYUSCULAS
     else if  (string.charCodeAt(i) >= 65  && string.charCodeAt(i) <= 90){
     let ascii = (string.charCodeAt(i) - 65 + offset )%26 + 65;
@@ -37,19 +56,7 @@ document.getElementById('boton1').addEventListener('click', function(){
       }
         }         
          })
+*/
+         
 
-//DESCIFRAR
-document.getElementById('boton2').addEventListener('click', function(){
-    let string = mensaje.value;
-    let offset = parseInt(desplaza.value);
-    for(let i =0; i < string.length; i++){
-        if(string.charCodeAt(i)==32){
-            resultado.innerHTML += " ";
-        }else{
-        if (string.charCodeAt(i) >= 65  && string.charCodeAt(i) <= 90) {
-        let ascii = (string.charCodeAt(i) - 90 - offset )%26 + 90;
-        resultado.innerHTML += String.fromCharCode(ascii)
-        } 
-    }          
-    }   
-})
+
